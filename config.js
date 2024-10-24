c91bcec9c497079f78112dc2759324c026dd521f
HerokuSL <ddilisha78@gmail.com>
2024-10-24 14:54:36 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,22 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_MSG: process.env.ALIVE_MSG || "Hi iam back now",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/cad7038fe82e47f79c609.jpg",
 MONGODB: process.env.MONGODB || "Put Mongodb Url",
+AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN !== undefined ? process.env.AUTO_STATUS_SEEN === 'true' : true, 
+AUTO_DL: process.env.AUTO_DL !== undefined ? process.env.AUTO_DL === 'true' : false,
+AUTO_READ: process.env.AUTO_READ !== undefined ? process.env.AUTO_READ === 'true' : false,
+AUTO_TYPING: process.env.AUTO_TYPING !== undefined ? process.env.AUTO_TYPING === 'true' : false,
+AUTO_RECORDING: process.env.AUTO_RECORDING !== undefined ? process.env.AUTO_RECORDING === 'true' : false,
+ALWAYS_ONLINE: process.env.ALWAYS_ONLINE !== undefined ? process.env.ALWAYS_ONLINE === 'true' : false,
+AUTO_REACT: process.env.AUTO_REACT !== undefined ? process.env.AUTO_REACT === 'true' : false,
+   /*auto block only for 212 */
+AUTO_BLOCK: process.env.AUTO_BLOCK !== undefined ? process.env.AUTO_BLOCK === 'true' : true,
+  
+  
+REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
+NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
+MODE: process.env.MODE || "public",
+OWNER_NAME: process.env.OWNER_NAME || "Dilishs",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "94775713391",
+GEMINI_KEY: process.env.GEMINI_KEY || "AIzaSyCUPaxfIdZawsKZKqCqJcC-GWiQPCXKTDc",
+WELCOME: process.env.WELCOME !== undefined ? process.env.WELCOME === 'true' : false, 
 };
