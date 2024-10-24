96a096563c63033c71334e1eb5f341f1acf3bcc8
HerokuSL <ddilisha78@gmail.com>
2024-10-24 15:02:20 +0530

    Update mediafire.js

--- plugins/mediafire.js
+++ plugins/mediafire.js
@@ -1,8 +1,8 @@
 // MEDIAFIRE DOWNLOAD COMMAND
 
-
-const { cmd } = require('../command')
 const { fetchJson } = require('../lib/functions')
+const config = require('../config')
+const { cmd, commands } = require('../command')
 
 const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
 
