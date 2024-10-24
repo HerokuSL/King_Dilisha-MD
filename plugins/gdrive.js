0c743f6e19d6a7baf1214b6a2988fa855babf00b
HerokuSL <ddilisha78@gmail.com>
2024-10-24 17:47:31 +0530

    Create gdrive.js

---
+++ plugins/gdrive.js
@@ -0,0 +1,7 @@
+// MEDIAFIRE DOWNLOAD COMMAND
+
+const { fetchJson } = require('../lib/functions')
+const config = require('../config')
+const { cmd, commands } = require('../command')
+
+const apilink = 'https://dark-yasiya-api-new.vercel.app' // API LINK ( DO NOT CHANGE THIS!! )
